3deac3929e53b5196e308dd52c36ca5b2f2d7f8e
Your Name <you@example.com>
2018-12-08 12:51:42 +0530

    project started

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"github.com/aloksinghal/redi-shard/handlers"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ping/", handler)
+	r.HandleFunc("/get/{key}",keyGetHandler)
+
+}
